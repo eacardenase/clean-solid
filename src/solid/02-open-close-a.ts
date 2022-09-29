560d9a3b5add1b745eb859fd36db8d2fd2eac0ba
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-09-29 11:27:12 -0500

    SOLID: Refactor using a class to abstract http functionality

--- src/solid/02-open-close-a.ts
+++ src/solid/02-open-close-a.ts
@@ -1,9 +1,10 @@
 import { PhotosService, PostService, TodoService } from './02-open-close-b';
+import { HttpClient } from './02-open-close-c';
 
 (async () => {
-    const todoService = new TodoService();
-    const postService = new PostService();
-    const photosService = new PhotosService();
+    const todoService = new TodoService(new HttpClient());
+    const postService = new PostService(new HttpClient());
+    const photosService = new PhotosService(new HttpClient());
 
     const todos = await todoService.getTodoItems();
     const posts = await postService.getPosts();

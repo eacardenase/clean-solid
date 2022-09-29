d444c0d6f1fff5d8a1240ed6f58936c9fb87670a
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-09-29 16:33:56 -0500

    SOLID: Refactor using ISP

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,39 +1,40 @@
 interface Bird {
-    fly(): void;
     eat(): void;
-    run(): void;
+}
+
+interface FlyingBird {
+    fly(): number;
+}
+
+interface SwimmerBird {
     swim(): void;
 }
 
-class Tucan implements Bird {
-    public fly(): void {}
-    public eat(): void {}
-    public run(): void {}
-    public swim(): void {}
+interface RunningBird {
+    run(): void;
 }
 
-class Hummingbird implements Bird {
-    public fly(): void {}
-    public eat(): void {}
-    public run(): void {}
-    public swim(): void {}
-}
-
-class Ostrich implements Bird {
-    public fly(): void {
-        throw new Error('Esta ave no vuela');
+class Tucan implements Bird, FlyingBird {
+    public fly(): number {
+        return new Date().getTime();
     }
     public eat(): void {}
-    public run(): void {}
-    public swim(): void {}
 }
 
-class Penguin implements Bird {
-    public fly(): void {
-        throw new Error('Esta ave no vuela');
+class Hummingbird implements Bird, FlyingBird {
+    public fly(): number {
+        return new Date().getTime();
     }
     public eat(): void {}
+}
+
+class Ostrich implements Bird, RunningBird {
+    public eat(): void {}
     public run(): void {}
+}
+
+class Penguin implements Bird, SwimmerBird {
+    public eat(): void {}
     public swim(): void {}
 }
 
